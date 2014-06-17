f39be0d7149ca80356639a4a8fb28e681a6dab9f
Abhishek Balaji <abhishek.balaji2@gmail.com>
2014-06-17 21:53:45 +0530

    Created a file with basic arithmetic operators

---
+++ numbers.rb
@@ -0,0 +1,16 @@
+=begin
+Anything typed over here is not considered
+as code by ruby. SO it can be used to comment stuff.
+=end
+addition=0
+addition=1+2
+subtraction=0
+subtraction=6-5
+multiplication=1
+multiplication=90*87
+division=1
+division=90/18
+puts addition
+puts subtraction
+puts multiplication
+puts division
